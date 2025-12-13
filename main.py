1e47d31707dd89268efb172b68977fb000f94ffe
mhimavamshi <himavamshi1212@gmail.com>
2025-12-13 22:55:01 +0530

    initial backend app

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
-def main():
-    print("Hello from backend!")
+import uvicorn
 
+from contextlib import asynccontextmanager
+from fastapi import FastAPI
+
+from database import init_db
+
+shared_resources = {}
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    print("Application starting up...")
+    print("Initializing database...")
+    await init_db()
+    yield
+    print("Application shutting down...")
+    shared_resources.clear() 
+
+app = FastAPI(lifespan=lifespan)
+
+@app.get("/up")
+def read_root():
+    return "online"
 
 if __name__ == "__main__":
-    main()
+    uvicorn.run(app)
